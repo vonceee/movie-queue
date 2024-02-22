ed4b522e07c09a75d7403886acaa6cebef0fa521
vontheworst <152864596+vonceee@users.noreply.github.com>
2024-02-23 00:50:44 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,33 @@
 <?php
+    // Start the session
     session_start();
-    if(isset($_SESSION["user"])){
-        header("Location: index.php");
+        if(isset($_SESSION["user"])){
+            header("Location: index.php");
+        }
+
+    // Check if the login form is submitted
+    if(isset($_POST["login"])){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        require_once "database.php";
+        $sql = "SELECT * FROM user WHERE email='$email'";
+        $result = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        if ($user) {
+            if (password_verify($password, $user["password"])) {
+                // Sets the user session
+                $_SESSION["user"] = $user;
+                // Redirect to the game.php page
+                header("Location: index.php");
+                exit(); // Stop script execution after redirect
+            } else {
+                // Password does not match
+                $errorMessage = "Password does not match";
+            }
+        } else {
+            // Email does not match
+            $errorMessage = "Email does not match";
+        }
     }
 ?>
 
@@ -32,29 +58,6 @@
     <!-- Image and text -->
 
     <div class="container">
-        <?php
-
-            if(isset($_POST["login"])){
-                $email=$_POST["email"];
-                $password=$_POST["password"];
-                    require_once "database.php";
-                    $sql="SELECT * FROM tbl_online_account WHERE email='$email'";
-                    $result=mysqli_query($conn, $sql);
-                    $user=mysqli_fetch_array($result, MYSQLI_ASSOC);
-                    if ($user) {
-                        if (password_verify($password, $user["password"])) {
-                            $_SESSION["user"]="yes";
-                            header("Location: index.php");
-                            die();
-                        } else {
-                            echo "<div class='alert alert-danger'> Password does not match</div>";
-                        }
-                    } else {
-                        echo "<div class='alert alert-danger'>Email does not match </div>";
-                    }
-                }
-        ?>
-
         <form action="login.php" method="post">
             <div class="form-group">
                 <input type="email" name="email" class="form-control" placeholder="Your Email" required>
